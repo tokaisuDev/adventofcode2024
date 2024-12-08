743de211cef0d057f985d965635d7ae8d5140e98
Nam <toymyorange@gmail.com>
2024-12-08 20:39:58 +0700

    Day 6 (correct version, whoops)

--- 6/6.py
+++ 6/6.py
@@ -30,8 +30,10 @@ with open("6.txt") as r:
 cols = len(o[0])
 mode = int(input("Which half? "))
 ans = 0
+tos = set() 
 while 0 <= curr[0] < rows and 0 <= curr[1] < cols:
     if not visited[curr[0]][curr[1]]:
+        tos.add(tuple(curr))
         ans += 1
         visited[curr[0]][curr[1]] = True
     next = [curr[0]+dir[0], curr[1]+dir[1]]
@@ -41,30 +43,31 @@ while 0 <= curr[0] < rows and 0 <= curr[1] < cols:
             dir = poss_dirs[turns_cnt % 4]
     curr = [curr[0]+dir[0], curr[1]+dir[1]]
 res = 0
-
-for i in range(rows):
-    for j in range(cols):
-        if not o[i][j] and [i, j] != starting_pos:
-            o[i][j] = True
-            v = set()
-            curr = starting_pos
-            dir = poss_dirs[0]
-            cnt = 0
-            loop = False
-            while 0 <= curr[0] < rows and 0 <= curr[1] < cols:
-                next = [curr[0]+dir[0], curr[1]+dir[1]]
-                if 0 <= next[0] < rows and 0 <= next[1] < cols and o[next[0]][next[1]]:
-                    cnt += 1
-                    dir = poss_dirs[cnt % 4]
-                else:
-                    curr = next
-                if tuple(curr+dir) in v:
-                    loop = True
-                    break
-                else:
-                    v.add(tuple(curr+dir))
-            if loop:
-                res += 1
+# pt.2
+for ob in tos:
+    if ob == starting_pos:
+        continue
+    curr = starting_pos
+    turns_cnt = 0
+    dir = poss_dirs[0]
+    loop = False
+    v = set()
+    o[ob[0]][ob[1]] = True
+    while 0 <= curr[0] < rows and 0 <= curr[1] < cols:
+        next = [curr[0]+dir[0], curr[1]+dir[1]]
+        if 0 <= next[0] < rows and 0 <= next[1] < cols and (o[next[0]][next[1]]):
+            turns_cnt += 1
+            dir = poss_dirs[turns_cnt % 4]
+        else:
+            curr = next
+        if tuple(curr+dir) in v:
+            loop = True
+            break
+        else:
+            v.add(tuple(curr+dir))
+    if loop:
+        res += 1
+    o[ob[0]][ob[1]] = False
 if mode == 1:
     print(ans)
 elif mode == 2:
